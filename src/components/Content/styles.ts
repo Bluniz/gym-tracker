45ceb85df2c10e316d430a941789c86491e062f7
Lucas Rosa <lucas.rosa@bankly.com.br>
2023-12-07 19:36:30 -0300

    chore: add flex 1 on container

--- src/components/Content/styles.ts
+++ src/components/Content/styles.ts
@@ -1,10 +1,10 @@
-import { StyleSheet } from 'react-native';
+import {StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
   content: {
     paddingHorizontal: 20,
     paddingBottom: 100,
 
+    flex: 1,
   },
- 
 });
