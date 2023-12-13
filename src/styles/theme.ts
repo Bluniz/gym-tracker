7c291d3c431c9f0997b9f6e445afa8b5552f7618
Lucas Rosa <lucas.rosa@bankly.com.br>
2023-12-13 14:47:40 -0300

    chore: improve colors organization

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,28 +1,30 @@
 import {systemColorScheme} from '../utils';
 
+const commonColors = {
+  black500: '#121214',
+  black50: 'rgba(18,18,20,0.2)',
+  white: '#FFFF',
+  primary: '#B80C09',
+  primaryLight: 'rgba(184, 12, 9, 0.70)',
+};
+
 export const Theme = {
   dark: {
     colors: {
       background: '#202024',
-      primary: '#B80C09',
-      primaryLight: 'rgba(184, 12, 9, 0.70)',
-      text: '#f2f2f2',
       backgroundMedium: '#323238',
       backgroundLight: '#7C7C8A',
-      black500: '#121214',
-      white: '#FFFF',
+      text: '#f2f2f2',
+      ...commonColors,
     },
   },
   light: {
     colors: {
       background: '#FFFF',
-      primary: '#B80C09',
-      primaryLight: 'rgba(184, 12, 9, 0.70)',
       text: '#202024',
       backgroundMedium: '#f2f2f2',
       backgroundLight: '#7C7C8A',
-      black500: '#121214',
-      white: '#FFFF',
+      ...commonColors,
     },
   },
 };
