da3cd8f0227fd145d8b16d9e6f3e86823a4fc9d9
Lucas Rosa <lucassantosrosa51@gmail.com>
2024-11-13 19:47:35 -0300

    feat: refactor CreateExerciseScreen to use Container and ScreenHeader components

--- src/app/(app)/exercises/createExercises.tsx
+++ src/app/(app)/exercises/createExercises.tsx
@@ -1,11 +1,17 @@
+import { Container } from '@/src/components/Container';
+import { ScreenHeader } from '@/src/components/ScreenHeader';
 import { Box } from '@/src/components/ui/box';
+import { Button, ButtonIcon } from '@/src/components/ui/button';
+import { Heading } from '@/src/components/ui/heading';
+import { HStack } from '@/src/components/ui/hstack';
 import { Text } from '@/src/components/ui/text';
-import Constants from 'expo-constants';
+import { router } from 'expo-router';
+import { MoveLeft } from 'lucide-react-native';
 
 export default function CreateExerciseScreen() {
   return (
-    <Box style={{ paddingTop: Constants.statusBarHeight }}>
-      <Text>Criar</Text>
-    </Box>
+    <Container>
+      <ScreenHeader title="Criar exercicios" />
+    </Container>
   );
 }
