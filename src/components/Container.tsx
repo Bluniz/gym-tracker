039aa4e0854c8ea342bb66cd905748552687a75a
Lucas Rosa <lucassantosrosa51@gmail.com>
2024-11-25 19:03:42 -0300

    feat: add Container component

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,8 +1,23 @@
+import Animated, { Easing, FadeIn, FadeOut } from 'react-native-reanimated';
 import { Box, IBoxProps } from './ui/box';
 import Constants from 'expo-constants';
 
-interface ContainerProps extends IBoxProps {}
+interface ContainerProps extends IBoxProps {
+  animate?: boolean;
+  animateDuration?: number;
+}
 
-export function Container(props: ContainerProps) {
-  return <Box {...props} style={{ paddingTop: Constants.statusBarHeight }} />;
+const AnimatedBox = Animated.createAnimatedComponent(Box);
+
+export function Container({ animate, animateDuration = 300, ...props }: ContainerProps) {
+  return (
+    <AnimatedBox
+      {...props}
+      style={{ paddingTop: Constants.statusBarHeight }}
+      {...(animate && {
+        entering: FadeIn.duration(animateDuration).easing(Easing.inOut(Easing.quad)),
+        exiting: FadeOut.duration(animateDuration).easing(Easing.inOut(Easing.quad)),
+      })}
+    />
+  );
 }
