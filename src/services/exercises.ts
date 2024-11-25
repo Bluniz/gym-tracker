b2d6466c82ed6a6f44747ba33c1a40d5f7b412f5
Lucas Rosa <lucassantosrosa51@gmail.com>
2024-11-25 19:04:06 -0300

    feat: add createExercise service

--- src/services/exercises.ts
+++ src/services/exercises.ts
@@ -1,10 +1,47 @@
 import { Tables } from '@/database.types';
 import { supabaseClient } from './supabase';
 
+type CreateExerciseInput = {
+  name: string;
+  description?: string;
+  photo_url?: string;
+  exercise_type: string[];
+  user_id: string;
+};
+
 export const getExercises = async (userId: string) => {
   return supabaseClient
     .from('exercises')
     .select()
     .eq('user_id', userId)
-    .returns<Tables<'exercises'>[]>();
+    .returns<Tables<'exercises'>[]>()
+    .throwOnError();
+};
+
+export const getExerciseTypes = async () => {
+  return supabaseClient
+    .from('exercises_types')
+    .select()
+    .returns<Tables<'exercises_types'>[]>()
+    .throwOnError();
+};
+
+export const createExercise = ({
+  name,
+  user_id,
+  photo_url,
+  description,
+  exercise_type,
+}: CreateExerciseInput) => {
+  return supabaseClient
+    .from('exercises')
+    .insert({
+      name,
+      user_id,
+      photo_url,
+      description,
+      exercise_type,
+    })
+    .single()
+    .throwOnError();
 };
