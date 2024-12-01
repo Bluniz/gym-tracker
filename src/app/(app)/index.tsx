c413332d790ed713c51b8da79b2af0e73d3ab9a8
Lucas Rosa <lucassantosrosa51@gmail.com>
2024-12-01 19:27:54 -0300

    feat: integrate Container and ScreenHeader components into Home page layout

--- src/app/(app)/index.tsx
+++ src/app/(app)/index.tsx
@@ -1,3 +1,5 @@
+import { Container } from '@/src/components/Container';
+import { ScreenHeader } from '@/src/components/ScreenHeader';
 import { Button, ButtonText } from '@/src/components/ui/button';
 import { Center } from '@/src/components/ui/center';
 import { Heading } from '@/src/components/ui/heading';
@@ -6,11 +8,19 @@ import { useAuth } from '@/src/contexts/authContext';
 export default function Home() {
   const { signOut } = useAuth();
   return (
-    <Center className="h-full w-full flex-1 bg-slate-800">
-      <Heading>Logado</Heading>
-      <Button onPress={signOut}>
-        <ButtonText>Sair</ButtonText>
-      </Button>
-    </Center>
+    <Container animate>
+      <ScreenHeader
+        title="Treinos"
+        description="O que vamos treinar hoje?"
+        canGoBack={false}
+        containerClassname="pt-2"
+      />
+      <Center className="h-full">
+        <Heading>Logado</Heading>
+        <Button onPress={signOut}>
+          <ButtonText>Sair</ButtonText>
+        </Button>
+      </Center>
+    </Container>
   );
 }
