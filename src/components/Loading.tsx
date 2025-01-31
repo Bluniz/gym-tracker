888788194b5da4c14058163d609b6812dfd3ece9
Lucas Rosa <lucassantosrosa51@gmail.com>
2025-01-31 17:07:36 -0300

    feat: enhance Loading component to accept custom className for styling

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,10 +1,16 @@
 import colors from 'tailwindcss/colors';
 import { Center } from './ui/center';
 import { Spinner } from './ui/spinner';
+import { ViewProps } from 'react-native';
+import clsx from 'clsx';
 
-export const Loading = () => {
+interface LoadingProps {
+  className?: ViewProps['className'];
+}
+
+export const Loading = ({ className }: LoadingProps) => {
   return (
-    <Center className="h-full w-full">
+    <Center className={clsx('h-full w-full', className)}>
       <Spinner size="large" color={colors.red[700]} />
     </Center>
   );
